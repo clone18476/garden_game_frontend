4295e4cfd4c959f954b49e1746815ba2b3cac57a
TSephanya Brun <tsbrun@icloud.com>
2021-04-22 16:55:03 -0400

    basic GET request to populate seed bank with flowers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const endPoint = "http://localhost:3000/api/v1/plants"
 
 document.addEventListener('DOMContentLoaded', () => {
     createStartGameButton();
+    populateSeedBank();
 
     document.querySelector('button#start-game-button').addEventListener('click', () => startGame())
 })
@@ -23,24 +24,32 @@ function startGame() {
     alert('Get ready to garden! Click any grid to plant a flower.')
 }
 
+function populateSeedBank() {
+    let seedBank = document.querySelector('div.main-child.seed-bank')
+
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(plants => {
+        plants.data.forEach(plant => {
+            let plantDiv = document.createElement('div')
+            plantDiv.classList.add('flower')
+            let img = document.createElement('img')
+            img.src = plant.attributes.img_url
+
+            let par1 = document.createElement('p')
+            par1.innerText = `Name: ${plant.attributes.name}`
+            let par2 = document.createElement('p')
+            par2.innerText = `Time to Grow: ${plant.attributes.time_to_grow} minutes`
+
+            plantDiv.appendChild(img)
+            plantDiv.appendChild(par1)
+            plantDiv.appendChild(par2)
+
+            seedBank.appendChild(plantDiv)
+        })
+    })
+}
+
 // part of plant_flower => collect all the cells in the grid programatically (so you can add new levels) and add click event listeners => document.querySelectorAll('td')
-
-// function getPlants() {
-//     fetch(endPoint) // returns a promise
-//     .then(response => response.json()) // parse promise into json
-//     .then(plants => { // do something with the json
-//         console.log(plants);
-//         plants.data.forEach(plant => {
-//             // const plantMarkup = 
-//             // <div> 
-//             //     <h3>${plant.attributes.name}</h3>
-//             // </div> ;
-
-//             // document.querySelector('#plants-container').innerHTML += plantMarkup
-
-//             document.querySelector('#plants-container').innerHTML += plant.attributes.name
-//         })
-//     })
-// }
 
 // NOTE: event.detail is how you access the click count
